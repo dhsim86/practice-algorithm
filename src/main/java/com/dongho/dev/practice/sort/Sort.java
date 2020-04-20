0ea60efe9db73b490221af7f6def1edf4b2f8d13
Dongho Sim <dhsim86@gmail.com>
2020-04-20 17:53:39 +0900

    practice: sort 2

--- src/main/java/com/dongho/dev/practice/sort/Sort.java
+++ src/main/java/com/dongho/dev/practice/sort/Sort.java
@@ -1,5 +1,7 @@
 package com.dongho.dev.practice.sort;
 
+import java.util.Arrays;
+
 public class Sort {
 
     public static void selectionSort(int[] data) {
@@ -40,6 +42,86 @@ public class Sort {
                     data[j + 1] = tmp;
                 }
             }
+        }
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        int[] sortedArray = new int[array.length];
+
+        System.out.print("merge: ");
+        for (int i = start; i < mid; i++) {
+            System.out.print("[" + array[i] + "]");
+        }
+        System.out.print(" + ");
+        for (int i = mid; i < end; i++) {
+            System.out.print("[" + array[i] + "]");
+        }
+        System.out.println("");
+
+        int target = start;
+        int i = start;
+        int j = mid;
+        while (i < mid && j < end) {
+            if (array[i] <= array[j]) {
+                sortedArray[target++] = array[i++];
+            } else if (array[j] < array[i]) {
+                sortedArray[target++] = array[j++];
+            }
+        }
+
+        while (i < mid) {
+            sortedArray[target++] = array[i++];
+        }
+        while (j < end) {
+            sortedArray[target++] = array[j++];
+        }
+
+        for (int x = start; x < end; x++) {
+            array[x] = sortedArray[x];
+        }
+    }
+
+    public static void mergeSort(int[] data, int start, int end) {
+        if (start < end - 1) {
+            int mid = (start + end) / 2;
+
+            mergeSort(data , start, mid);
+            mergeSort(data, mid, end);
+            merge(data, start, mid, end);
+        }
+    }
+
+    private static int partition(int[] data, int start, int end) {
+        int pivot = start;
+        int left = start;
+        int right = end - 1;
+
+        while (left < right) {
+            while (left <= right && data[pivot] >= data[left]) {
+                left++;
+            }
+            while (left <= right && data[pivot] <= data[right]) {
+                right--;
+            }
+
+            if (left < right) {
+                int tmp = data[left];
+                data[left] = data[right];
+                data[right] = tmp;
+            }
+        }
+        int tmp = data[pivot];
+        data[pivot] = data[right];
+        data[right] = tmp;
+
+        return right;
+    }
+
+    public static void quickSort(int[] data, int start, int end) {
+        if (start < end - 1) {
+            int pivot = partition(data, start, end);
+            quickSort(data, start, pivot);
+            quickSort(data, pivot + 1, end);
         }
     }
 
--- src/test/java/com/dongho/dev/practice/sort/SortTest.java
+++ src/test/java/com/dongho/dev/practice/sort/SortTest.java
@@ -15,7 +15,7 @@ public class SortTest {
         Sort.selectionSort(data);
 
         // then
-        assertThat(data).containsExactlyInAnyOrder(1, 3, 4, 6, 7, 9);
+        assertThat(data).containsExactly(1, 3, 4, 6, 7, 9);
     }
 
     @Test
@@ -27,7 +27,7 @@ public class SortTest {
         Sort.insertionSort(data);
 
         // then
-        assertThat(data).containsExactlyInAnyOrder(1, 3, 4, 6, 7, 9);
+        assertThat(data).containsExactly(1, 3, 4, 6, 7, 9);
     }
 
     @Test
@@ -39,7 +39,31 @@ public class SortTest {
         Sort.bubbleSort(data);
 
         // then
-        assertThat(data).containsExactlyInAnyOrder(1, 3, 4, 6, 7, 9);
+        assertThat(data).containsExactly(1, 3, 4, 6, 7, 9);
+    }
+
+    @Test
+    public void mergeSortTest() {
+        // given
+        int[] data = new int[] {9, 3, 4, 1, 7, 1, 10, 11, 2, 6, 12, 5, 8, 14, 15, 13};
+
+        // when
+        Sort.mergeSort(data, 0, data.length);
+
+        // then
+        assertThat(data).containsExactly(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    }
+
+    @Test
+    public void quickSortTest() {
+        // given
+        int[] data = new int[] {9, 3, 4, 1, 7, 1, 10, 11, 2, 6, 12, 5, 8, 14, 15, 13};
+
+        // when
+        Sort.quickSort(data, 0, data.length);
+
+        // then
+        assertThat(data).containsExactly(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
     }
 
 }
