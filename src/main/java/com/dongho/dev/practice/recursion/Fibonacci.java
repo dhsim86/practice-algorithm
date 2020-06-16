8653a613d1d5c4ab3e0bc012ca7874ca19c53018
Dongho Sim <dhsim86@gmail.com>
2020-06-16 14:36:51 +0900

    slf log 추가
    fibonacci with memoization

--- src/main/java/com/dongho/dev/practice/recursion/Fibonacci.java
+++ src/main/java/com/dongho/dev/practice/recursion/Fibonacci.java
@@ -13,4 +13,26 @@ public class Fibonacci {
         return fib(n - 2) + fib(n - 1);
     }
 
+    public static int[] fibMemoizationArray;
+
+    public static void initFibMemoizationList(int n) {
+        fibMemoizationArray = new int[n + 1];
+    }
+
+    public static int fibMemoization(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        if (fibMemoizationArray[n] != 0) {
+            return fibMemoizationArray[n];
+        }
+
+        fibMemoizationArray[n] = fibMemoization(n - 1) + fibMemoization(n - 2);
+        return fibMemoizationArray[n];
+    }
+
 }
