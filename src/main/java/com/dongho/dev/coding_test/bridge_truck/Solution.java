9a7cb1ecba56634df3413f058bcf0d610be46960
Dongho Sim <dhsim86@gmail.com>
2020-06-22 14:52:32 +0900

    Coding Test (다리를 지나는 트럭): https://programmers.co.kr/learn/courses/30/lessons/42583

---
+++ src/main/java/com/dongho/dev/coding_test/bridge_truck/Solution.java
@@ -0,0 +1,61 @@
+package com.dongho.dev.coding_test.bridge_truck;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+    static class BridgeTruck {
+        private int weight;
+        private int remainLength;
+
+        public BridgeTruck(int weight, int remainLength) {
+            this.weight = weight;
+            this.remainLength = remainLength;
+        }
+
+        public void decreaseRemainLength() {
+            --this.remainLength;
+        }
+
+        public boolean isRemainLengthZero() {
+            return this.remainLength == 0;
+        }
+
+        public int getWeight() {
+            return this.weight;
+        }
+    }
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+        Queue<BridgeTruck> bridgeQueue = new LinkedList<>();
+        Queue<Integer> truckQueue = new LinkedList<>(
+                IntStream.of(truck_weights).boxed().collect(Collectors.toList()));
+
+        while (true) {
+            if (bridgeQueue.isEmpty() && truckQueue.isEmpty()) {
+                break;
+            }
+
+            bridgeQueue.stream().forEach(BridgeTruck::decreaseRemainLength);
+            BridgeTruck bridgeTruck = bridgeQueue.peek();
+            if (bridgeTruck != null && bridgeTruck.isRemainLengthZero()) {
+                bridgeQueue.remove();
+            }
+
+            int currentBridgeWeight = bridgeQueue.stream().map(BridgeTruck::getWeight).reduce(0, (a, b) -> a + b);
+            Integer truckWeight = truckQueue.peek();
+
+            if (truckWeight != null && currentBridgeWeight + truckWeight <= weight) {
+                truckQueue.remove();
+                bridgeQueue.add(new BridgeTruck(truckWeight, bridge_length));
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/com/dongho/dev/coding_test/bridge_truck/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.dongho.dev.coding_test.bridge_truck;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+    @Test
+    public void SolutionTest() {
+        // given
+        int bridgeLength= 2;
+        int weight = 10;
+        int[] truckWeights = new int[]{7, 4, 5, 6};
+
+        Solution solution = new Solution();
+
+        // when
+        int result = solution.solution(bridgeLength, weight, truckWeights);
+
+        // then
+        assertThat(result).isEqualTo(8);
+    }
+
+}
