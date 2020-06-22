61fd4f71777fc7eb0d1757be2875c0f166a92fac
Dongho Sim <dhsim86@gmail.com>
2020-06-22 18:18:24 +0900

    Coding Test (종이접기): https://programmers.co.kr/learn/courses/30/lessons/62049

---
+++ src/test/java/com/dongho/dev/coding_test/origami/SolutionTest.java
@@ -0,0 +1,24 @@
+package com.dongho.dev.coding_test.origami;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+    @Test
+    public void solutionTest() {
+        // given
+        Solution solution = new Solution();
+
+        // when
+        int[] result0 = solution.solution(1);
+        int[] result1 = solution.solution(2);
+        int[] result2 = solution.solution(3);
+
+        // then
+        assertThat(result0).containsExactly(0);
+        assertThat(result1).containsExactly(0, 0, 1);
+        assertThat(result2).containsExactly(0, 0, 1, 0, 0, 1, 1);
+    }
+}
