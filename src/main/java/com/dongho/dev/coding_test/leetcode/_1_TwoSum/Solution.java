8a69d13fc739070163c112f19e723f41f7e447e8
Dongho Sim <dhsim86@gmail.com>
2021-04-26 18:50:08 +0900

    leetcode #1.TwoSum

---
+++ src/main/java/com/dongho/dev/coding_test/leetcode/_1_TwoSum/Solution.java
@@ -0,0 +1,21 @@
+package com.dongho.dev.coding_test.leetcode._1_TwoSum;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
