427e3a2d7de620a1aab0b54b7d58b37af87d3730
Dongho Sim <dhsim86@gmail.com>
2021-05-31 18:50:45 +0900

    leetcode #303. Range Sum Query - Immutable

---
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_303_RangeSumQueryImmutable/NumArray.java
@@ -0,0 +1,21 @@
+package com.dongho.dev.coding_test.leetcode.dynamic_programming._303_RangeSumQueryImmutable;
+
+public class NumArray {
+
+    private int[] nums;
+    
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+}
