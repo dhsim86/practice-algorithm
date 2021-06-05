acd1c1de99355207fcc3b90517da9ff44cdbf4d1
Dongho Sim <dhsim86@gmail.com>
2021-06-05 16:35:16 +0900

    leetcode #91. Decode Ways

---
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_91_DecodeWays/Solution.java
@@ -0,0 +1,55 @@
+package com.dongho.dev.coding_test.leetcode.dynamic_programming._91_DecodeWays;
+
+public class Solution {
+
+    private String numberString;
+    private int[][] dp;
+
+    private boolean isValid(int start, int end) {
+        if (start == end) {
+            return false;
+        }
+
+        int number = Integer.parseInt(numberString.substring(start, end));
+
+        if (number < 1 || number > 26) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int getWays(int start, int end) {
+        if (start >= end) {
+            return 0;
+        }
+
+        if (start >= numberString.length() && end >= numberString.length()) {
+            return 0;
+        }
+
+        if (dp[start][end] > 0) {
+            return dp[start][end];
+        }
+
+        if (isValid(start, end)) {
+            if (end >= numberString.length()) {
+                dp[start][end] = 1;
+            } else {
+                dp[start][end] = getWays(start, end + 1) + getWays(end, end + 1);
+            }
+        } else {
+            dp[start][end] = 0;
+        }
+
+        return dp[start][end];
+    }
+    
+    public int numDecodings(String s) {
+        dp = new int[s.length() + 1][s.length() + 1];
+        numberString = s;
+
+        return getWays(0, 1);
+    }
+
+}
---
+++ src/test/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_91_DecodeWays/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.dongho.dev.coding_test.leetcode.dynamic_programming._91_DecodeWays;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        // given
+
+        // when
+        int result0 = new Solution().numDecodings("12");
+        int result1 = new Solution().numDecodings("226");
+
+        // then
+        assertThat(result0).isEqualTo(2);
+        assertThat(result1).isEqualTo(3);
+    }
+    
+}
