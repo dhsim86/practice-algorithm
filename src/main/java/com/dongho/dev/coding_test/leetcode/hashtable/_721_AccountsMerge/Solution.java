fd7f9f1fc5ba95e04083f2a99385552d4d2ff800
Dongho Sim <dhsim86@gmail.com>
2021-08-03 19:09:56 +0900

    fixed

--- src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_721_AccountsMerge/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_721_AccountsMerge/Solution.java
@@ -2,6 +2,7 @@ package com.dongho.dev.coding_test.leetcode.hashtable._721_AccountsMerge;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -9,61 +10,69 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    private List<String> getCandidateNameList(Map<String, Set<String>> mailMap, String name) {
-        return mailMap.keySet().stream()
-            .filter(key -> key.startsWith(name))
-            .collect(Collectors.toList());
+    private List<Set<String>> merge(List<Set<String>> nameSetList, List<String> mailList) {
+        List<Integer> idxList = new ArrayList<>();
+
+        for (int i = 0; i < nameSetList.size(); i++) {
+            Set<String> nameSet = nameSetList.get(i);          
+
+            for (String mail : mailList) {
+                if (nameSet.contains(mail)) {
+                    idxList.add(i);
+                    break;
+                }
+            }
+        }
+
+        Set<String> merged = new HashSet<>();
+        for (Integer idx : idxList) {
+            merged.addAll(nameSetList.get(idx));
+        }
+        merged.addAll(mailList);
+
+        List<Set<String>> result = new ArrayList<>();
+        result.add(merged);
+
+        for (int i = 0; i < nameSetList.size(); i++) {
+            if (idxList.contains(i) == false) {
+                result.add(nameSetList.get(i));
+            }
+        }
+        return result;
     }
 
     public List<List<String>> accountsMerge(List<List<String>> accounts) {    
-        Map<String, Set<String>> mailMap = new HashMap<>();
-        int id = 0;
+        Map<String, List<Set<String>>> mailMap = new HashMap<>();
 
         for (List<String> account : accounts) {
             String name = account.get(0);
             List<String> mailList = account.subList(1, account.size());
 
-            List<String> candidateNameList = getCandidateNameList(mailMap, name);
-
-            if (candidateNameList.size() == 0) {
-                String nameKey = name + "_" + id;
-                mailMap.put(nameKey, mailList.stream().collect(Collectors.toSet()));
+            if (mailMap.get(name) == null) {
+                Set<String> nameSet = mailList.stream().collect(Collectors.toSet());
+                List<Set<String>> nameSetList = new ArrayList<>();
+                nameSetList.add(nameSet);
+                mailMap.put(name, nameSetList);
             } else {
-                boolean hasMail = false;
-                String matched = null;
-
-                LOOP: for (String candidateName : candidateNameList) {
-                    Set<String> existMailSet = mailMap.get(candidateName);
-                    for (String mail : mailList) {
-                        if (existMailSet.contains(mail)) {
-                            hasMail = true;
-                            matched = candidateName;
-                            
-                            break LOOP;
-                        }
-                    }
-                }
-
-                if (hasMail) {
-                    Set<String> existMailSet = mailMap.get(matched);
-                    existMailSet.addAll(mailList);
-                    mailMap.put(matched, existMailSet);
-                } else {
-                    String nameKey = name + "_" + id;
-                    mailMap.put(nameKey, mailList.stream().collect(Collectors.toSet()));
-                }
+                List<Set<String>> nameSetList = mailMap.get(name);
+                List<Set<String>> newNameSetList = merge(nameSetList, mailList);
+                mailMap.put(name, newNameSetList);
             }
-            id++;
         }
 
         List<List<String>> accountList = new ArrayList<>();
-        for (String nameKey : mailMap.keySet()) {
-            List<String> accountInfo = new ArrayList<>();
-            accountInfo.add(nameKey.split("_")[0]);
-            accountInfo.addAll(mailMap.get(nameKey).stream().sorted().collect(Collectors.toList()));
-            accountList.add(accountInfo);
+        for (String name : mailMap.keySet()) {
+            List<Set<String>> nameSetList = mailMap.get(name);
+
+            for (Set<String> nameSet : nameSetList) {
+                List<String> accountInfo = new ArrayList<>();
+                accountInfo.add(name);
+                accountInfo.addAll(nameSet.stream().sorted().collect(Collectors.toList()));
+                accountList.add(accountInfo);
+            }
         }
 
         return accountList;
     }
+    
 }
