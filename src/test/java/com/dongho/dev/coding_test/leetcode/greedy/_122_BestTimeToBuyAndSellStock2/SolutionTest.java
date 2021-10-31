26762768e1dac70dc44bb14a2e8737dfc5ec1c6e
Dongho Sim <dhsim86@gmail.com>
2021-10-31 17:56:36 +0900

    test

--- src/test/java/com/dongho/dev/coding_test/leetcode/greedy/_122_BestTimeToBuyAndSellStock2/SolutionTest.java
+++ src/test/java/com/dongho/dev/coding_test/leetcode/greedy/_122_BestTimeToBuyAndSellStock2/SolutionTest.java
@@ -8,6 +8,7 @@ public class SolutionTest {
     @Test
     public void test() {
         // given
+        
         int[] prices1 = {7, 1, 5, 3, 6, 4};
         int[] prices2 = {1, 2, 3, 4, 5};
         int[] prices3 = {7, 6, 4, 3, 1};
