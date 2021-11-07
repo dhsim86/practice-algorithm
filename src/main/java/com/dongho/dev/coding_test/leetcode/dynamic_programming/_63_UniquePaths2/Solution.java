50a4e9a0103b136915696a6b199bb9ce3f270443
Dongho Sim <dhsim86@gmail.com>
2021-11-07 22:58:03 +0900

    leetcode #63. Unique Path 2 (update)

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_63_UniquePaths2/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_63_UniquePaths2/Solution.java
@@ -1,48 +1,42 @@
 package com.dongho.dev.coding_test.leetcode.dynamic_programming._63_UniquePaths2;
 
-import java.util.Arrays;
-
 public class Solution {
 
-    private int[][] grid;
-    private int xEndPos;
-    private int yEndPos;
     private int[][] dp;
+    private int[][] obstacleGrid;
+    private int xEnd;
+    private int yEnd;
 
-    public int getCount(int y, int x) {
-        if (dp[y][x] >= 0) {
-            return dp[y][x];
+    private int getCount(int x, int y) {
+        if (dp[x][y] > 0) {
+            return dp[x][y];
         }
 
-        if (grid[y][x] == 1) {
-            return dp[y][x] = 0;
+        if (obstacleGrid[x][y] == 1) {
+            return 0;
         }
 
-        if (y == yEndPos - 1 && x == xEndPos - 1) {
+        if (x == xEnd - 1 && y == yEnd - 1) {
             return 1;
         }
 
-        if (y == yEndPos - 1) {
-            dp[y][x] = getCount(y, x + 1);
-        } else if (x == xEndPos - 1) {
-            dp[y][x] = getCount(y + 1, x);
+        if (x == xEnd - 1) {
+            dp[x][y] = getCount(x, y + 1);
+        } else if (y == yEnd - 1) {
+            dp[x][y] = getCount(x + 1, y);
         } else {
-            dp[y][x] = getCount(y, x + 1) + getCount(y + 1, x);
+            dp[x][y] = getCount(x + 1, y) + getCount(x, y + 1);
         }
 
-        return dp[y][x];
+        return dp[x][y];
     }
-    
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        grid = obstacleGrid;
-        yEndPos = grid.length;
-        xEndPos = grid[0].length;
+        dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        xEnd = dp.length;
+        yEnd = dp[0].length;
 
-        dp = new int[yEndPos][xEndPos];
-
-        for (int i = 0; i < yEndPos; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        this.obstacleGrid = obstacleGrid;
 
         return getCount(0, 0);
     }
