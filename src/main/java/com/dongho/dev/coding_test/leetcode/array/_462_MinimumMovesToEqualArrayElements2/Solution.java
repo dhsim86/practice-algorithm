fdfcd5f8f064a59d7fff7ac9786bd7bd2bfdd794
Dongho Sim <dhsim86@gmail.com>
2021-11-13 14:35:53 +0900

    leetcode #462. Minimum Moves to Equal Array Elements 2

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_462_MinimumMovesToEqualArrayElements2/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_462_MinimumMovesToEqualArrayElements2/Solution.java
@@ -7,14 +7,19 @@ import java.util.Arrays;
 public class Solution {
 
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);
-        int medianIndex = nums.length / 2;
+        nums = Arrays.stream(nums)
+            .sorted()
+            .toArray();
+
+        int mid = nums.length / 2;
         int count = 0;
 
-        for (int num : nums) {
-            count += Math.abs(nums[medianIndex] - num);
+        for (int i = 0; i < nums.length; i++) {
+            if (mid == i) {
+                continue;
+            }
+            count += Math.abs(nums[mid] - nums[i]);
         }
-
         return count;
     }
 
