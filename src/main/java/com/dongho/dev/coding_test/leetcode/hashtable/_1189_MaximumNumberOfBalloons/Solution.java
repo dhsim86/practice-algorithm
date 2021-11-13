8a8de95b4b8829ba4383e3706b47d1a3d52f0d9c
Dongho Sim <dhsim86@gmail.com>
2021-11-13 23:22:19 +0900

    leetcode #1189. Maximum Number of Balloons (update)

--- src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_1189_MaximumNumberOfBalloons/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_1189_MaximumNumberOfBalloons/Solution.java
@@ -1,41 +1,35 @@
 package com.dongho.dev.coding_test.leetcode.hashtable._1189_MaximumNumberOfBalloons;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-    private String TEMPLATE = "balloon";
-    private Map<Character, Integer> map = new HashMap<>();
-
     public int maxNumberOfBalloons(String text) {
-        for (char c : TEMPLATE.toCharArray()) {
-            map.put(c, 0);
-        }
-        
-        for (int i = 0; i < text.length(); i++) {
-            Character c = text.charAt(i);
+        Map<Character, Integer> ballonMap = "balloon".chars()
+            .mapToObj(c -> (char) c)
+            .distinct()
+            .collect(Collectors.toMap(c -> c, c -> 0));
             
-            if (TEMPLATE.contains(c.toString())) {
-                Integer count = map.get(c);
-
-                if (count != null) {
-                    map.put(c, count + 1);
-                } else {
-                    map.put(c, 1);
-                }
+        for (char c : text.toCharArray()) {
+            if (ballonMap.get(c) != null) {
+                ballonMap.put(c, ballonMap.get(c) + 1);
             }
         }
 
-        Integer minCount = map.values().stream()
-            .min(Integer::compare)
-            .get();
-        Integer lCount = map.get('l');
-        Integer oCount = map.get('o');
-
-        int count = Math.min(minCount, lCount / 2);
-        count = Math.min(count, oCount / 2);
-
-        return count;
+        return ballonMap.entrySet().stream()
+            .map(e -> {
+                if (e.getKey() == 'l' || e.getKey() == 'o') {
+                    e.setValue(e.getValue() / 2);
+                }
+                return e;
+            })
+            .sorted(Comparator.comparing(Entry<Character, Integer>::getValue))
+            .map(Entry::getValue)
+            .findFirst()
+            .orElse(0);
     }
+
 }
