c68220edcd183289a7b2ab6bdf87f75560f71438
Dongho Sim <dhsim86@gmail.com>
2021-11-13 15:00:01 +0900

    leetcode #690. Employee Importance (update)

--- src/main/java/com/dongho/dev/coding_test/leetcode/breadth_firsh_search/_690_EmployeeImportance/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/breadth_firsh_search/_690_EmployeeImportance/Solution.java
@@ -1,8 +1,9 @@
 package com.dongho.dev.coding_test.leetcode.breadth_firsh_search._690_EmployeeImportance;
 
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Employee {
     public int id;
@@ -12,27 +13,24 @@ class Employee {
 
 public class Solution {
 
-    public Map<Integer, Employee> employeeMap;
-
-    private int getImportance(Employee employee) {
-        List<Integer> subordinates = employee.subordinates;
-        int importance = 0;
-        for (Integer subordinate : subordinates) {
-            importance += getImportance(employeeMap.get(subordinate));
-        }
-        return importance + employee.importance;
-    }
-    
     public int getImportance(List<Employee> employees, int id) {
-        if (employeeMap == null) {
-            employeeMap = new HashMap<>();
+        Map<Integer, Employee> map = employees.stream()
+            .collect(Collectors.toMap(e -> e.id, e -> e));
+        
+        int sum = 0;
 
-            for (Employee employee : employees) {
-                employeeMap.put(employee.id, employee);
-            }
+        List<Integer> subOrdinates = new LinkedList<>();
+        subOrdinates.add(id);
+
+        while(subOrdinates.isEmpty() == false) {
+            Integer subid = subOrdinates.remove(0);
+            Employee employee = map.get(subid);
+
+            sum += employee.importance;
+            subOrdinates.addAll(employee.subordinates);
         }
 
-        return getImportance(employeeMap.get(id));
+        return sum;
     }
 
 }
