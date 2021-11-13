0fef747a074ac2b0516f51c63c57f453a0d36d3d
Dongho Sim <dhsim86@gmail.com>
2021-11-13 23:01:35 +0900

    leetcode #987. Vertical Order Traversal of a Binary Tree (update)

--- src/main/java/com/dongho/dev/coding_test/leetcode/tree/_987_VerticalOrderTraversalOfBinaryTree/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/tree/_987_VerticalOrderTraversalOfBinaryTree/Solution.java
@@ -29,80 +29,63 @@ class TreeNode {
 
 public class Solution {
 
-    private Map<Integer, Queue<NodeInfo>> map;
+    Map<Integer, Queue<NodeInfo>> map = new HashMap<>();
 
-    private static class NodeInfo implements Comparable<NodeInfo> {
+    private class NodeInfo implements Comparable<NodeInfo> {
         int val;
         int row;
 
-        public NodeInfo(TreeNode node, int row, int col) {
-            this.val = node.val;
+        public NodeInfo(int val, int row) {
+            this.val = val;
             this.row = row;
         }
 
         @Override
-        public int compareTo(NodeInfo other) {
-            if (row < other.row) {
-                return -1;
+        public int compareTo(NodeInfo o) {
+            if (row != o.row) {
+                return Integer.compare(row, o.row);
             }
-
-            if (row > other.row) {
-                return 1;
-            }
-
-            return Integer.compare(val, other.val);
+            return Integer.compare(val, o.val);
         }
     }
 
-    private Queue<NodeInfo> getNodeQueue(int column) {
-        Queue<NodeInfo> queue = map.get(column);
-
-        if (queue == null) {
-            queue = new PriorityQueue<>();
-            map.put(column, queue);
-        }
-
-        return queue;
-    }
-
-    private void traverse(TreeNode node, int row, int column) {
+    private void traverse(TreeNode node, int row, int col) {
         if (node == null) {
             return;
         }
 
-        traverse(node.left, row + 1, column - 1);
+        Queue<NodeInfo> queue = map.get(col);
+        if (queue == null) {
+            queue = new PriorityQueue<>();
+            map.put(col, queue);
+        }
 
-        Queue<NodeInfo> queue = getNodeQueue(column);
-        queue.add(new NodeInfo(node, row, column));
+        queue.add(new NodeInfo(node.val, row));
 
-        traverse(node.right, row + 1, column + 1);
+        traverse(node.left, row + 1, col - 1);
+        traverse(node.right, row + 1, col + 1);
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-
-        map = new HashMap<>();
         traverse(root, 0, 0);
 
-        List<List<Integer>> result = new ArrayList<>();
-
-        List<Integer> keyList = map.keySet().stream()
+        List<Integer> keySet = map.keySet().stream()
             .sorted()
             .collect(Collectors.toList());
 
-        for (int key : keyList) {
-            List<Integer> subList = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (Integer key : keySet) {
+            List<Integer> newList = new ArrayList<>();
             Queue<NodeInfo> queue = map.get(key);
 
             while (queue.isEmpty() == false) {
-                subList.add(queue.poll().val);
+                newList.add(queue.poll().val);
             }
-
-            result.add(subList);
+            result.add(newList);
         }
 
         return result;
     }
+
 }
