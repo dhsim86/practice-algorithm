33c328bd79f74feb151ea0b27dda1feedf5213d1
Dongho Sim <dhsim86@gmail.com>
2021-11-13 21:32:36 +0900

    leetcode #771. Jewels and Stones (update)

--- src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_771_JewelsAndStones/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_771_JewelsAndStones/Solution.java
@@ -1,23 +1,24 @@
 package com.dongho.dev.coding_test.leetcode.hashtable._771_JewelsAndStones;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Solution {
-    
-    public int numJewelsInStones(String jewels, String stones) {
-        Map<Character, Integer> map = jewels.chars().mapToObj(c -> (char)c)
-            .collect(Collectors.toMap(c -> c, c -> 0));
 
-        for (int i = 0; i < stones.length(); i++) {
-            Character stone = stones.charAt(i);
-            
-            if (map.containsKey(stone)) {
-                map.put(stone, map.get(stone) + 1);
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Integer> jewelMap = new HashMap<>();
+
+        for (char c : jewels.toCharArray()) {
+            jewelMap.put(c, 0);
+        }
+
+        for (char c : stones.toCharArray()) {
+            if (jewelMap.containsKey(c)) {
+                jewelMap.put(c, jewelMap.get(c) + 1);
             }
         }
 
-        return map.values().stream()
+        return jewelMap.values().stream()
             .reduce(0, Integer::sum);
     }
 
