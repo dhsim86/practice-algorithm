6b62a32157af78e36f1c00c1472cdd7c8aa72201
Dongho Sim <dhsim86@gmail.com>
2021-11-14 17:43:31 +0900

    leetcode #1475. Final Prices With a Special Discount in a Shop

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_1475_FinalPricesWithSpecialDiscount/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_1475_FinalPricesWithSpecialDiscount/Solution.java
@@ -1,22 +1,24 @@
 package com.dongho.dev.coding_test.leetcode.array._1475_FinalPricesWithSpecialDiscount;
 
 public class Solution {
-    public int[] finalPrices(int[] prices) {
-        int[] result = new int[prices.length];
 
-        for (int i = 0; i < prices.length; i++) {
-            int price = prices[i];
-            int discount = 0;
+    private int getDiscount(int[] prices, int index) {
+        int discount = 0;
 
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[j] <= price) {
-                    discount = prices[j];
-                    break;
-                }
+        for (int i = prices.length - 1; i > index; i--) {
+            if (prices[index] >= prices[i]) {
+                discount = prices[i];
             }
-            result[i] = price - discount;
         }
 
-        return result;
+        return discount;
     }
+
+    public int[] finalPrices(int[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            prices[i] = prices[i] - getDiscount(prices, i);
+        }
+        return prices;
+    }
+
 }
