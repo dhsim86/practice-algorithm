bb2c8b6a898b262de4fc0f4e20c6a8ac637a0d11
Dongho Sim <dhsim86@gmail.com>
2021-11-29 14:56:33 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_64_MinimumPathSum/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_64_MinimumPathSum/Solution.java
@@ -3,37 +3,38 @@ package com.dongho.dev.coding_test.leetcode.dynamic_programming._64_MinimumPathS
 public class Solution {
 
     private int[][] grid;
-    private int[][] dp;
-    private int yEnd;
-    private int xEnd;
+    private int m;
+    private int n;
 
-    private int getPathSum(int y, int x) {
-        if (dp[y][x] >= 1) {
+    // dp[y][x] = min(dp[y + 1][x], dp[y][x + 1])
+    // y == m && x == m => return
+    private int[][] dp;
+
+    private int walk(int y, int x) {
+        if (y >= m || x >= n) {
+            return Short.MAX_VALUE;
+        }
+
+        if (dp[y][x] > 0) {
             return dp[y][x];
         }
 
-        if (y == yEnd && x == xEnd) {
+        if (y == m - 1 && x == n - 1) {
             return grid[y][x];
         }
 
-        if (y < yEnd && x < xEnd) {
-            dp[y][x] = grid[y][x] +
-                Math.min(getPathSum(y, x + 1), getPathSum(y + 1, x));
-        } else if (y < yEnd) {
-            dp[y][x] = grid[y][x] + getPathSum(y + 1, x);
-        } else if (x < xEnd) {
-            dp[y][x] = grid[y][x] + getPathSum(y, x + 1);
-        }
+        dp[y][x] = grid[y][x] + Math.min(walk(y + 1, x), walk(y, x + 1));
         return dp[y][x];
     }
 
+
     public int minPathSum(int[][] grid) {
         this.grid = grid;
-        this.dp = new int[grid.length][grid[0].length];
-        this.yEnd = grid.length - 1;
-        this.xEnd = grid[0].length - 1;
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.dp = new int[m][n];
 
-        return getPathSum(0, 0);
+        return walk(0, 0);
     }
 
 }
