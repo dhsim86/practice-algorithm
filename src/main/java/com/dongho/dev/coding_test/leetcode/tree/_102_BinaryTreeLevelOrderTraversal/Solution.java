91dcd77939f1de0e568a8e3b3f2621444f2401b6
Dongho Sim <dhsim86@gmail.com>
2021-11-29 17:15:51 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/tree/_102_BinaryTreeLevelOrderTraversal/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/tree/_102_BinaryTreeLevelOrderTraversal/Solution.java
@@ -1,6 +1,7 @@
 package com.dongho.dev.coding_test.leetcode.tree._102_BinaryTreeLevelOrderTraversal;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -19,40 +20,39 @@ class TreeNode {
 }
 
 public class Solution {
-    
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
 
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
+        
         queue.add(root);
 
         while (queue.isEmpty() == false) {
-            List<TreeNode> tempList = new LinkedList<>();
+            List<Integer> list = new ArrayList<>();
+            Queue<TreeNode> nextQueue = new LinkedList<>();
 
             while (queue.isEmpty() == false) {
                 TreeNode node = queue.poll();
+                list.add(node.val);
 
-                if (node != null) {
-                    tempList.add(node);
+                if (node.left != null) {
+                    nextQueue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    nextQueue.add(node.right);
                 }
             }
 
-            List<Integer> levelResult = new ArrayList<>();
-            
-            while (tempList.isEmpty() == false) {
-                TreeNode node = tempList.remove(0);
-
-                levelResult.add(node.val);
-                queue.add(node.left);
-                queue.add(node.right);
-            }
-
-            if (levelResult.isEmpty() == false) {
-                result.add(levelResult);
-            }
+            result.add(list);
+            queue = nextQueue;
         }
-        
+
         return result;
-    }
+    } 
 
 }
