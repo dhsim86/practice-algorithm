dc75f71172a5d4953d40521686a8eb2b9dcf06cc
Dongho Sim <dhsim86@gmail.com>
2022-01-01 21:35:14 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_53_MaximumSubArray/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_53_MaximumSubArray/Solution.java
@@ -1,27 +1,18 @@
 package com.dongho.dev.coding_test.leetcode.array._53_MaximumSubArray;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Solution {
 
     public int maxSubArray(int[] nums) {
         // refers: https://leetcode.com/problems/maximum-subarray/discuss/1560342/Maximum-Subarray-in-Java
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
 
-        int sum = nums[0];
-        int res = nums[0];
-        
-        log.info("start for array: {}", nums);
-        log.info("sum: {}, res: {}", sum, res);
-
-        for (int i = 1; i < nums.length; i++) {
-            sum = Integer.max(nums[i], sum + nums[i]);
-            res = Integer.max(res, sum);
-
-            log.info("sum: {}, res: {}", sum, res);
+        for (int i = 0; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum + nums[i]);
+            max = Math.max(max, sum);
         }
 
-        return res;
+        return max;
     }
     
 }
