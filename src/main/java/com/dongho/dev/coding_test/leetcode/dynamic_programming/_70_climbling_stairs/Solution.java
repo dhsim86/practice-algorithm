5cd715a9069257649d9e864b4308820168e73729
Dongho Sim <dhsim86@gmail.com>
2022-01-01 22:28:50 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_70_climbling_stairs/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_70_climbling_stairs/Solution.java
@@ -1,31 +1,31 @@
 package com.dongho.dev.coding_test.leetcode.dynamic_programming._70_climbling_stairs;
 
 public class Solution {
-    
-    // dp[n] = dp[n - 1] + dp[n - 2]
 
-    // dp[1] = 1
-    // dp[2] = 2
-    
-    // dp[3] = dp[1] + dp[2]
-    //  1 + 2
-    //  2 + 1
+    // dp[n] = (dp[n - 2] + dp[n - 1])
+    // n == 0 => count++;
+    private int[] dp;
+
+    private int walk(int n) {
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (dp[n] > 0) {
+            return dp[n];
+        }
+
+        dp[n] = walk(n - 2) + walk(n - 1);
+        return dp[n];
+    }
 
     public int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
-        }
-
-        int dp[] = new int[n + 1];
-
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];
+        dp = new int[n + 1];
+        return walk(n);
     }
 
 }
