39f7c689a1809ea1f28f54780d31bee84aa09e28
Dongho Sim <dhsim86@gmail.com>
2022-01-01 20:43:22 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_1_TwoSum/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_1_TwoSum/Solution.java
@@ -6,19 +6,16 @@ import java.util.Map;
 public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
-
         Map<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
-                result[0] = map.get(target - nums[i]);
-                result[1] = i;
+                return new int[] { map.get(target - nums[i]), i };
             }
             map.put(nums[i], i);
         }
 
-        return result;
+        return new int[0];
     }
 
 }
