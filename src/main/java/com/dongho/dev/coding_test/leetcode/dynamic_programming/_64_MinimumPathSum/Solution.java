151f7d492464769b4ea50a55ce1ba360ec1a6491
Dongho Sim <dhsim86@gmail.com>
2022-01-01 22:22:29 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_64_MinimumPathSum/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_64_MinimumPathSum/Solution.java
@@ -2,39 +2,28 @@ package com.dongho.dev.coding_test.leetcode.dynamic_programming._64_MinimumPathS
 
 public class Solution {
 
-    private int[][] grid;
-    private int m;
-    private int n;
-
-    // dp[y][x] = min(dp[y + 1][x], dp[y][x + 1])
-    // y == m && x == m => return
     private int[][] dp;
 
-    private int walk(int y, int x) {
-        if (y >= m || x >= n) {
-            return Short.MAX_VALUE;
+    private int walk(int y, int x, int[][] grid) {
+        if (y >= grid.length || x >= grid[0].length) {
+            return 10000;
+        }
+
+        if (y == grid.length - 1 && x == grid[0].length - 1) {
+            return grid[y][x];
         }
 
         if (dp[y][x] > 0) {
             return dp[y][x];
         }
 
-        if (y == m - 1 && x == n - 1) {
-            return grid[y][x];
-        }
-
-        dp[y][x] = grid[y][x] + Math.min(walk(y + 1, x), walk(y, x + 1));
+        dp[y][x] = grid[y][x] + Math.min(walk(y + 1, x, grid), walk(y, x + 1, grid));
         return dp[y][x];
     }
 
-
     public int minPathSum(int[][] grid) {
-        this.grid = grid;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        this.dp = new int[m][n];
-
-        return walk(0, 0);
+        dp = new int[grid.length][grid[0].length];
+        return walk(0, 0, grid);   
     }
 
 }
