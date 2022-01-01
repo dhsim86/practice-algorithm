afc73f58734eefa9086f2153bfdfd964b43c07d0
Dongho Sim <dhsim86@gmail.com>
2022-01-01 22:05:59 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_62_UniquePaths/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_62_UniquePaths/Solution.java
@@ -2,36 +2,30 @@ package com.dongho.dev.coding_test.leetcode.dynamic_programming._62_UniquePaths;
 
 public class Solution {
 
+    // dp = dp[y + 1][x] + dp[y][x + 1];
+    // y == m - 1 && x == n - 1 => 1
     private int[][] dp;
-    private int m;
-    private int n;
-
-    // dp[y][x] = dp[y + 1][x] + dp[y][x + 1];
-    // y == m && x == n ? 1 : 0
 
     private int walk(int y, int x) {
-        if (y == m - 1 && x == n - 1) {
-            return 1;
-        }
-
-        if (y >= m || x >= n) {
+        if (y >= dp.length || x >= dp[0].length) {
             return 0;
         }
 
-        if (dp[y][x] > 0) {
-            return dp[y][x];
+        if (y == dp.length - 1 && x == dp[0].length - 1) {
+            return 1;
         }
 
         dp[y][x] = walk(y + 1, x) + walk(y, x + 1);
         return dp[y][x];
     }
 
-    public int uniquePaths(int m, int n) {
-        this.dp = new int[m][n];
-        this.m = m;
-        this.n = n;
 
-        return walk(0, 0);
+
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        
+        int result = walk(0, 0);
+        return result;
     }
 
 }
