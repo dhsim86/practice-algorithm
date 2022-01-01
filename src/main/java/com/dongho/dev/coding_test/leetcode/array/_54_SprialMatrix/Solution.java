4053ad348641c8f9a96543140d84ced842bf6330
Dongho Sim <dhsim86@gmail.com>
2022-01-01 21:57:06 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_54_SprialMatrix/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_54_SprialMatrix/Solution.java
@@ -5,35 +5,6 @@ import java.util.List;
 
 public class Solution {
 
-    private boolean[][] isVisited;
-
-    private static class Position {
-        int x;
-        int y;
-
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public static Position getNextPosition(Direction cur, Position curPosition) {
-            int x = curPosition.x;
-            int y = curPosition.y;
-
-            switch (cur) {
-            case LEFT:
-                return new Position(++x, y);
-            case DOWN:
-                return new Position(x, ++y);
-            case RIGHT:
-                return new Position(--x, y);
-            case UP:
-                return new Position(x, --y);
-            }
-            return null;
-        }
-    }
-
     private enum Direction {
         LEFT,
         DOWN,
@@ -45,7 +16,7 @@ public class Solution {
             case LEFT:
                 return DOWN;
             case DOWN:
-                return RIGHT;
+                return RIGHT; 
             case RIGHT:
                 return UP;
             case UP:
@@ -53,52 +24,70 @@ public class Solution {
             }
             return null;
         }
+    }
 
-        public static NextInfo getNextInfo(Direction cur, boolean[][] isVisited, int curX, int curY) {
-            Direction nextDirection = cur.getNext();
+    private static class Point {
+        int y;
+        int x;
+        Direction direction;
 
-            Position next = Position.getNextPosition(cur, new Position(curX, curY));
+        Point(int y, int x, Direction direction) {
+            this.y = y;
+            this.x = x;
+            this.direction = direction;
+        }
 
-            if (next.x >= 0 && next.y >= 0 && next.x < isVisited[0].length && next.y < isVisited.length && isVisited[next.y][next.x] == false) {
-                return new NextInfo(cur, next.x, next.y);
+        private boolean isReachable(boolean[][] visited) {
+            if (this.y < 0 || this.y >= visited.length ||
+                this.x < 0 || this.x >= visited[0].length) {
+                return false;
             }
 
-            next = Position.getNextPosition(nextDirection, new Position(curX, curY));
-
-            if (next.x >= 0 && next.y >= 0 && next.x < isVisited[0].length && next.y < isVisited.length && isVisited[next.y][next.x] == false) {
-                return new NextInfo(nextDirection, next.x, next.y);
+            if (visited[this.y][this.x]) {
+                return false;
             }
 
+            return true;
+        }
+
+        public Point calculateNext() {
+            switch(this.direction) {
+            case LEFT:
+                return new Point(y, x + 1, this.direction);
+            case DOWN:
+                return new Point(y + 1, x, this.direction);
+            case RIGHT:
+                return new Point(y, x - 1, this.direction);
+            case UP:
+                return new Point(y - 1, x, this.direction);
+            }
             return null;
         }
     }
 
-    private static class NextInfo {
-        Direction direction;
-        int x;
-        int y;
-
-        public NextInfo(Direction direction, int x, int y) {
-            this.direction = direction;
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
     public List<Integer> spiralOrder(int[][] matrix) {
-        isVisited = new boolean[matrix.length][matrix[0].length];
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        int visitedCount = 0;
+        int maxCount = matrix.length * matrix[0].length;
+        Point point = new Point(0, 0, Direction.LEFT);
 
-        NextInfo next = new NextInfo(Direction.LEFT, 0, 0);
+        while (visitedCount < maxCount) {
+            visited[point.y][point.x] = true;
+            result.add(matrix[point.y][point.x]);
+            visitedCount++;
 
-        do {
-            isVisited[next.y][next.x] = true;
-            list.add(matrix[next.y][next.x]);
+            Point next = point.calculateNext();
 
-            next = Direction.getNextInfo(next.direction, isVisited, next.x, next.y);
-        } while (next != null);
+            if (next.isReachable(visited) == false) {
+                point.direction = point.direction.getNext();
+                next = point.calculateNext();
+            }
 
-        return list;
+            point = next;
+        }
+
+        return result;
     }
 
 }
