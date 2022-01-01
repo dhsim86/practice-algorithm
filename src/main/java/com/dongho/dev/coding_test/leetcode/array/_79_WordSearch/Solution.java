7626cb21b94e25c047151d09bac62f0e77d35d6d
Dongho Sim <dhsim86@gmail.com>
2022-01-02 02:22:07 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/array/_79_WordSearch/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/array/_79_WordSearch/Solution.java
@@ -2,41 +2,44 @@ package com.dongho.dev.coding_test.leetcode.array._79_WordSearch;
 
 public class Solution {
 
+    private String word;
     private char[][] board;
     private boolean[][] visited;
-    private String word;
 
-    private boolean evaluate(int y, int x, int cur) {
-        if (y < 0 || x < 0 || y >= board.length || x >= board[0].length || cur >= word.length()) {
+    private boolean visit(int y, int x, int cur) {
+        if (cur == word.length()) {
+            return true;
+        }
+
+        if (y < 0 || x < 0 || y >= board.length || x >= board[0].length) {
             return false;
         }
 
-        if (visited[y][x] || board[y][x] != word.charAt(cur)) {
+        if (visited[y][x]) {
             return false;
         }
 
         visited[y][x] = true;
+        boolean result = false;
 
-        if (board[y][x] == word.charAt(cur) && cur == word.length() - 1) {
-            visited[y][x] = false;
-            return true;
+        if (board[y][x] == word.charAt(cur)) {
+            result = visit(y, x + 1, cur + 1) || visit(y + 1, x, cur + 1) ||
+                visit(y, x - 1, cur + 1) || visit(y - 1, x, cur + 1);
         }
-
-        boolean result = evaluate(y, x + 1, cur + 1) || evaluate(y + 1, x, cur + 1) || evaluate(y, x - 1, cur + 1) || evaluate(y - 1, x, cur + 1);
 
         visited[y][x] = false;
 
         return result;
     }
-    
+
     public boolean exist(char[][] board, String word) {
-        this.board = board;
         this.word = word;
+        this.board = board;
         this.visited = new boolean[board.length][board[0].length];
 
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[0].length; x++) {
-                if (evaluate(y, x, 0)) {
+                if (visit(y, x, 0)) {
                     return true;
                 }
             }
