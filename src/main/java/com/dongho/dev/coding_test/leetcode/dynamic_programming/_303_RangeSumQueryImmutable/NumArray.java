95c737824431ec21b4bfc93cc68b05a7f0bf11c7
Dongho Sim <dhsim86@gmail.com>
2022-01-02 18:34:38 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_303_RangeSumQueryImmutable/NumArray.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_303_RangeSumQueryImmutable/NumArray.java
@@ -3,18 +3,16 @@ package com.dongho.dev.coding_test.leetcode.dynamic_programming._303_RangeSumQue
 public class NumArray {
 
     private int[] nums;
-    
+
     public NumArray(int[] nums) {
         this.nums = nums;
     }
-
+    
     public int sumRange(int left, int right) {
         int sum = 0;
-
         for (int i = left; i <= right; i++) {
             sum += nums[i];
         }
-
         return sum;
     }
 
