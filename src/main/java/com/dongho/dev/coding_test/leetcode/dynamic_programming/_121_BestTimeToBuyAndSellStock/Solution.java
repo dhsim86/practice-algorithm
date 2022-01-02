4042c6a52704338991b65fbe738e50d828188530
Dongho Sim <dhsim86@gmail.com>
2022-01-02 14:31:27 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_121_BestTimeToBuyAndSellStock/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/dynamic_programming/_121_BestTimeToBuyAndSellStock/Solution.java
@@ -3,17 +3,18 @@ package com.dongho.dev.coding_test.leetcode.dynamic_programming._121_BestTimeToB
 public class Solution {
 
     public int maxProfit(int[] prices) {
-        int min = 0;
-        int profit = 0;
+        int max = Integer.MIN_VALUE;
+        int buy = 0;
 
         for (int i = 0; i < prices.length; i++) {
-            profit = Math.max(profit, prices[i] - prices[min]);
-
-            if (prices[i] < prices[min]) {
-                min = i;
+            max = Math.max(max, prices[i] - prices[buy]);
+            
+            if (prices[i] < prices[buy]) {
+                buy = i;
             }
         }
-        return profit;
+
+        return max;
     }
 
 }
