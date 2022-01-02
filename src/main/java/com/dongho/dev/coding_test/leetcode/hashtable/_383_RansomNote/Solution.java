2de20ae508f017d5362eee97072877d97932b5d6
Dongho Sim <dhsim86@gmail.com>
2022-01-02 19:06:14 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_383_RansomNote/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_383_RansomNote/Solution.java
@@ -4,14 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> map = new HashMap<>();
 
         for (char c : ransomNote.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
         for (char c : magazine.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) - 1);
         }
@@ -19,5 +17,4 @@ public class Solution {
         return !map.values().stream()
             .anyMatch(v -> v > 0);
     }
-    
 }
