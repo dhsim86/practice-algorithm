a27b616458f3178f77ed6a747c7616b7e069f29c
Dongho Sim <dhsim86@gmail.com>
2022-01-02 17:06:38 +0900

    update

--- src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_169_MajorityElement/Solution.java
+++ src/main/java/com/dongho/dev/coding_test/leetcode/hashtable/_169_MajorityElement/Solution.java
@@ -8,19 +8,15 @@ public class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        int max = Integer.MIN_VALUE;
-        int major = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-             
-             if (max < map.get(nums[i])) {
-                 major = nums[i];
-                 max = map.get(nums[i]);
-             }
+        for (int i = 0 ; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        return major;
+        return map.entrySet().stream()
+            .filter(e -> e.getValue() > nums.length / 2)
+            .map(e -> e.getKey())
+            .findFirst()
+            .orElse(0);
     }
     
 }
